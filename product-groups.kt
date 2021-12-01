6ec6ca584ef538865902ce61b48899b2846a1b26
Janine <78605206+ja9labs@users.noreply.github.com>
2021-12-01 12:19:26 -0300

    Update product-groups.kt

--- product-groups.kt
+++ product-groups.kt
@@ -10,9 +10,9 @@ sealed class Grupos(name: String)
 
 class Grupo1 {
 
-    val frutas."180(dias)" = Citros, Maçã, Uva
-    val frutas."360(dias)" = Melão, Morango, Coco, Goiaba, Caqui, Mamão, Banana, Manga
-    val frutas."720(dias)" = Abacate, Abacaxi, Anonáceas, Cacau, Cupuaçu, Kiwi, Maracujá, Melancia, Romã, Açaí, Acerola, Amora, Ameixa, Caju, Carambola, Figo, Framboesa, Marmelo, Nectarina, Nêspera, Pêssego, Pitanga, Pêra e Mirtilo
+    val frutas."180(dias)" = Citros, Maça, Uva
+    val frutas."360(dias)" = Melao, Morango, Coco, Goiaba, Caqui, Mamao, Banana, Manga
+    val frutas."720(dias)" = Abacate, Abacaxi, Anonaceas, Cacau, Cupuacu, Kiwi, Maracuja, Melancia, Roma, Açaa, Acerola, Amora, Ameixa, Caju, Carambola, Figo, Framboesa, Marmelo, Nectarina, Nespera, Pessego, Pitanga, Pêra e Mirtilo
 }
 
 
@@ -21,23 +21,23 @@ class Grupo1 {
 class Grupo2 {
     val tipo2.180(dias) = Batata
     val tipo2.360(dias) = Cenoura, Batata-Doce, Beterraba, Cebola, Alho
-    val tipo2.720(dias) = Cará, Gengibre, Inhame, Mandioca, Mandioquinha-Salsa, Nabo, Rabanete, Batata-Yacon
+    val tipo2.720(dias) = Cara, Gengibre, Inhame, Mandioca, Mandioquinha-Salsa, Nabo, Rabanete, Batata-Yacon
 
 } 
 
 //hortaliças folhosas e ervas aromáticas frescas [Grupo3]
 class Grupo3 {
     val tipo3.180(dias) = Alface, Repolho
-    val tipo3.360(dias) = Couve, Agrião, Almeirão, Brócolis, Chicórea, Couve-Flor
-    val tipo3.720(dias) = Couve-Chinesa, Couve-De-Bruxelas, Espinafre, Rúcula, Alho-Poro, Cebolinha, Coentro, Manjericão, Salsa, Erva-Doce, Alecrim, Estragão, Manjerona, Salvia, Hortelã, Orégano, Mostarda, Acelga, Aipo; Aspargos
+    val tipo3.360(dias) = Couve, Agriao, Almeirão, Brocolis, Chicorea, Couve-Flor
+    val tipo3.720(dias) = Couve-Chinesa, Couve-De-Bruxelas, Espinafre, Rucula, Alho-Poro, Cebolinha, Coentro, Manjericao, Salsa, Erva-Doce, Alecrim, Estragao, Manjerona, Salvia, Hortela, Oregano, Mostarda, Acelga, Aipo; Aspargos
 }
 
 
 //hortaliças não folhosas [Grupo4]
 class Grupo4 {
     val tipo4.180(dias) = Tomate, Pepino
-    val tipo4.360(dias) = Pimentão, Abóbora, Abobrinha
-    val tipo4.720(dias) = Berinjela, Chuchu, Jiló, Maxixe, Pimenta, Quiabo
+    val tipo4.360(dias) = Pimentão, Abobora, Abobrinha
+    val tipo4.720(dias) = Berinjela, Chuchu, Jila, Maxixe, Pimenta, Quiabo
 }
 
 fun greetGrupos(Grupo1: frutas): String {
