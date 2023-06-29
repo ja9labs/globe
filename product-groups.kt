dc48d65db74b10bcb826f94b562c8b039043abd3
Janine <78605206+ja9labs@users.noreply.github.com>
2023-06-29 13:04:41 -0300

    Update product-groups.kt

--- product-groups.kt
+++ product-groups.kt
@@ -10,9 +10,9 @@ sealed class Grupos(name: String)
 
 class Grupo1 {
 
-    val frutas."180(dias)" = Citros, Maça, Uva
+    val frutas."180(dias)" = Citros, Maca, Uva
     val frutas."360(dias)" = Melao, Morango, Coco, Goiaba, Caqui, Mamao, Banana, Manga
-    val frutas."720(dias)" = Abacate, Abacaxi, Anonaceas, Cacau, Cupuacu, Kiwi, Maracuja, Melancia, Roma, Açaa, Acerola, Amora, Ameixa, Caju, Carambola, Figo, Framboesa, Marmelo, Nectarina, Nespera, Pessego, Pitanga, Pêra e Mirtilo
+    val frutas."720(dias)" = Abacate, Abacaxi, Anonaceas, Cacau, Cupuacu, Kiwi, Maracuja, Melancia, Roma, Acai, Acerola, Amora, Ameixa, Caju, Carambola, Figo, Framboesa, Marmelo, Nectarina, Nespera, Pessego, Pitanga, Pêra e Mirtilo
 }
 
 
@@ -26,17 +26,19 @@ class Grupo2 {
 } 
 
 //hortaliças folhosas e ervas aromáticas frescas [Grupo3]
+
 class Grupo3 {
     val tipo3.180(dias) = Alface, Repolho
-    val tipo3.360(dias) = Couve, Agriao, Almeirão, Brocolis, Chicorea, Couve-Flor
+    val tipo3.360(dias) = Couve, Agriao, Almeirao, Brocolis, Chicorea, Couve-Flor
     val tipo3.720(dias) = Couve-Chinesa, Couve-De-Bruxelas, Espinafre, Rucula, Alho-Poro, Cebolinha, Coentro, Manjericao, Salsa, Erva-Doce, Alecrim, Estragao, Manjerona, Salvia, Hortela, Oregano, Mostarda, Acelga, Aipo; Aspargos
 }
 
 
 //hortaliças não folhosas [Grupo4]
+
 class Grupo4 {
     val tipo4.180(dias) = Tomate, Pepino
-    val tipo4.360(dias) = Pimentão, Abobora, Abobrinha
+    val tipo4.360(dias) = Pimentao, Abobora, Abobrinha
     val tipo4.720(dias) = Berinjela, Chuchu, Jila, Maxixe, Pimenta, Quiabo
 }
 
